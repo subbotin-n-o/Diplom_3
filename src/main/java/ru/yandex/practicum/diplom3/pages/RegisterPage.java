95d44b3b420963f6349e46b0d1fa0945a57e6d59
subbotin-n-o <subbotin.n.o@gmail.com>
2023-02-07 23:30:48 +0400

    #14 update RegisterPage.java

--- src/main/java/ru/yandex/practicum/diplom3/pages/RegisterPage.java
+++ src/main/java/ru/yandex/practicum/diplom3/pages/RegisterPage.java
@@ -13,9 +13,9 @@ public class RegisterPage {
 
     public static final String FIELD_NAME = ".//form/fieldset[1]/div/div/input";
     public static final String FIELD_EMAIL = ".//form/fieldset[2]/div/div/input";
-    public static final String PASSWORD = "Пароль";
-    public static final String REGISTER = "Зарегистрироваться";
-    public static final String LOGIN = "Войти";
+    public static final String FIELD_PASSWORD = "Пароль";
+    public static final String REGISTER_BTN = "Зарегистрироваться";
+    public static final String LOGIN_BTN = "Войти";
     public static final String ERROR_MESSAGE = ".input__error";
     public static final String REGISTER_HEADER = ".Auth_login__3hAey > h2:nth-child(1)";
 
@@ -30,13 +30,13 @@ public class RegisterPage {
     @FindBy(how = How.XPATH, using = FIELD_EMAIL)
     private SelenideElement fieldEmail;
 
-    @FindBy(how = How.NAME, using = PASSWORD)
+    @FindBy(how = How.NAME, using = FIELD_PASSWORD)
     private SelenideElement fieldPassword;
 
-    @FindBy(how = How.LINK_TEXT, using = REGISTER)
+    @FindBy(how = How.LINK_TEXT, using = REGISTER_BTN)
     private SelenideElement registerButton;
 
-    @FindBy(how = How.LINK_TEXT, using = LOGIN)
+    @FindBy(how = How.LINK_TEXT, using = LOGIN_BTN)
     private SelenideElement loginButton;
 
     @FindBy(how = How.CSS, using = ERROR_MESSAGE)
