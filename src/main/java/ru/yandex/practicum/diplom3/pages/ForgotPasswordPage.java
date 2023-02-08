39b71cd5da8d8eb3addbe1856e5b61e85f490b8e
subbotin-n-o <subbotin.n.o@gmail.com>
2023-02-08 21:35:13 +0400

    #16 update ForgotPasswordPage.java

--- src/main/java/ru/yandex/practicum/diplom3/pages/ForgotPasswordPage.java
+++ src/main/java/ru/yandex/practicum/diplom3/pages/ForgotPasswordPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-import static com.codeborne.selenide.Condition.partialText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.page;
 
